b8c78fc7616594601a84da6530cf5c41b5cde1ce
KyungJae Min <minkyungjae0524@gmail.com>
2025-04-07 17:34:32 +0900

    Day11 Finished

--- 03_SQL/05_subquery.sql
+++ 03_SQL/05_subquery.sql
@@ -35,38 +35,97 @@ where  job_id = (select job_id from emp where emp_id = 120); -- 120번 직원의
 
 -- 직원_id(emp.emp_id)가 115번인 직원과 같은 업무(emp.job_id)를 하고 같은 부서(emp.dept_id)에 속한 직원들을 조회하시오.
 
+select job_id, dept_id from emp where emp_id = 115;
 
+select * from emp
+where    (job_id, dept_id) = (select job_id, dept_id 
+							  from emp 
+                              where emp_id = 115);
+
+-- select * from emp where (job_id, dept_id) = ('PU_MAN', 30);
+-- mysql은 지원하지만 안되는 dbms도 있다.
 
 -- 직원의 ID(emp.emp_id)가 150인 직원과 업무(emp.job_id)와 상사(emp.mgr_id)가 같은 직원들의 
 -- id(emp.emp_id), 이름(emp.emp_name), 업무(emp.job_id), 상사(emp.mgr_id) 를 조회
-
-
+select emp_id, emp_name, job_id
+from   emp
+where  (job_id, mgr_id) = (select job_id, mgr_id from emp where emp_id = 150);
 
 -- 직원들 중 급여(emp.salary)가 전체 직원의 평균 급여보다 적은 직원들의 id(emp.emp_id), 이름(emp.emp_name), 급여(emp.salary)를 조회. 
+select avg(salary) from emp;  -- 6517.906542
 
-
+select emp_id, emp_name, salary
+from   emp
+where salary < (select avg(salary) from emp)
+order by 3 desc;
 
 -- 부서직원들의 평균이 전체 직원의 평균(emp.salary) 이상인 부서의 이름(dept.dept_name), 평균 급여(emp.salary) 조회.
 -- 평균급여는 소숫점 2자리까지 나오고 통화표시($)와 단위 구분자 출력
 
+select d.dept_id, d.dept_name, avg(e.salary) as "평균급여"
+from   dept d join emp e on d.dept_id = e.dept_id
+group by d.dept_id, d.dept_name
+having avg(salary) >= (select avg(salary) from emp)
+order by 3
+;
+
+select d.dept_id,
+	   d.dept_name,
+       concat('$', round(avg(e.salary),2)) as "평균급여"   # 숫자와 string이 합쳐져서 정렬이 문자열로 처리됨
+from   dept d join emp e on d.dept_id = e.dept_id
+group by d.dept_id, d.dept_name
+having avg(salary) >= (select avg(salary) from emp)
+order by 3
+;
 
 
+select dept_id,
+	   dept_name,
+       concat('$', round(평균급여, 2)) as "평균급여"
+from (select d.dept_id, d.dept_name, avg(e.salary) as "평균급여"
+	  from   dept d join emp e on d.dept_id = e.dept_id
+	  group by d.dept_id, d.dept_name
+	 having avg(salary) >= (select avg(salary) from emp)
+	order by 3
+    ) as t;
+ 
 --  급여(emp.salary)가장 많이 받는 직원이 속한 부서의 이름(dept.dept_name), 위치(dept.loc)를 조회.
 
-
-
+select dept_name, loc
+from   dept
+where  dept_id = (select dept_id 
+				  from emp
+                  where salary = (select max(salary) from emp));
+                  
 -- Sales 부서(dept.dept_name) 의 평균 급여(emp.salary)보다 급여가 많은 직원들의 모든 정보를 조회.
 
+select * from emp
+where salary > (select avg(salary)
+			    from   dept d join emp e on d.dept_id = e.dept_id
+				where d.dept_name = 'Sales')
+order by salary asc;
 
+select avg(salary)
+from   dept d join emp e on d.dept_id = e.dept_id
+where d.dept_name = 'Sales';
 
 -- 전체 직원들 중 담당 업무 ID(emp.job_id) 가 'ST_CLERK'인 직원들의 평균 급여보다 적은 급여를 받는 직원들의 모든 정보를 조회. 
 -- 단 업무 ID가 'ST_CLERK'이 아닌 직원들만 조회. 
 
+select * from emp
+where salary < (select avg(salary) from emp where job_id = 'ST_CLERK')
+and   (job_id <> 'ST_CLERK' or job_id is null);  
+-- job_id is null을 안하면 job_id가 null인 행들이 조회가 안됨. null은 =, <> 모두 false.
 
 
 -- 업무(emp.job_id)가 'IT_PROG' 인 직원들 중 가장 많은 급여를 받는 직원보다 더 많은 급여를 받는 직원들의 id(emp.emp_id), 이름(emp.emp_name), 급여(emp.salary)를 급여 내림차순으로 조회.
 
+select emp_id, emp_name, salary
+from   emp
+where salary > (select max(salary) from emp where job_id = 'IT_PROG')
+order by 3 desc;
 
+select max(salary) from emp where job_id = 'IT_PROG';
 
 /* ----------------------------------------------
  다중행 서브쿼리
@@ -78,22 +137,48 @@ where  job_id = (select job_id from emp where emp_id = 120); -- 120번 직원의
 ------------------------------------------------*/
 -- 'Alexander' 란 이름(emp.emp_name)을 가진 관리자(emp.mgr_id)의 부하 직원들의 ID(emp_id), 이름(emp_name), 업무(job_id), 입사년도(hire_date-년도만출력), 급여(salary)를 조회
 
+select emp_id, emp_name, job_id, year(hire_date) as "입사년도", salary
+from   emp
+where  mgr_id in (select emp_id from emp where emp_name = 'Alexander');
+
+select emp_id, emp_name from emp where emp_name = 'Alexander'
+;
 
 --  부서 위치(dept.loc) 가 'New York'인 부서에 소속된 직원의 ID(emp.emp_id), 이름(emp.emp_name), 부서_id(emp.dept_id) 를 sub query를 이용해 조회.
+
+select emp_id, emp_name, dept_id
+from   emp
+where  dept_id in (select dept_id from dept where loc = 'New York');
+
+-- subquery where 절의 비교 컬럼(loc)이 unique가 아닐 경우 => 결과행이 여러개일 수 있다.
+-- 다중행 subquery 연산.
 
 
 -- 직원 ID(emp.emp_id)가 101, 102, 103 인 직원들 보다 급여(emp.salary)를 많이 받는 직원의 모든 정보를 조회.
 
+select * from emp
+where salary > all(select salary from emp where emp_id in (101, 102, 103));
+-- where salary > (select max(salary) from emp where emp_id in (101, 102, 103));
+
+select salary from emp where emp_id in (101, 102, 103);
 
 -- 직원 ID(emp.emp_id)가 101, 102, 103 인 직원들 중 급여가 가장 적은 직원보다 급여를 많이 받는 직원의 모든 정보를 조회.
 
+select * from emp
+where salary > any(select salary from emp where emp_id in (101, 102, 103));
 
 -- 최대 급여(job.max_salary)가 6000이하인 업무를 담당하는  직원(emp)의 모든 정보를 sub query를 이용해 조회.
 
+select * from emp
+where job_id in (select job_id from job where max_salary <= 6000);
 
+select job_id from job where max_salary <= 6000;
 -- 전체 직원들중 부서_ID(emp.dept_id)가 20인 부서의 모든 직원들 보다 급여(emp.salary)를 많이 받는 직원들의 정보를 sub query를 이용해 조회.
 
+select * from emp
+where salary > all(select salary from emp where dept_id = 20);
 
+select salary from emp where dept_id = 20; # 확인용
 
 /* *************************************************************************************************
 인라인 뷰(Inline View)
@@ -101,16 +186,21 @@ where  job_id = (select job_id from emp where emp_id = 120); -- 120번 직원의
 - 서브쿼리 결과를 테이블처럼 사용할 수 있다.
 * *************************************************************************************************/
 
--- 중복된 데이터 조회 
-
+-- 중복된 데이터 조회 (emp에서 여러 번 나오는 이름을 조회)
+select emp_name, cnt
+from (
+	select emp_name, count(*) as "cnt"
+	from   emp
+	group by emp_name  
+    order by cnt
+) t
+where cnt >= 2;
 
 /* *************************************************************************************************
 상관 쿼리
 - 메인쿼리문 테이블의 값을 where절의 subquery에서 참조한다.
 	- 메인 쿼리의 각 행마다 where의 subquery가 조회 대상인지 검사하면서 실행된다. 이때 현재 검사중인 그 행의 컬럼값을 subquery에서 사용한다.
     
-
-
 EXISTS, NOT EXISTS 연산자 (상관쿼리와 같이 사용된다)
 -- 서브쿼리의 결과를 만족하는 값이 존재하는지 여부를 확인하는 조건. 
 -- 조건을 만족하는 행이 여러개라도 한행만 있으면 더이상 검색하지 않는다.
@@ -127,8 +217,15 @@ EXISTS, NOT EXISTS 연산자 (상관쿼리와 같이 사용된다)
 
 -- 직원이 한명이상 있는 부서의 부서ID(dept.dept_id)와 이름(dept.dept_name), 위치(dept.loc)를 조회
 
+select d.dept_id, d.dept_name, d.loc
+from   dept d
+where  exists (select * from emp e where e.dept_id = d.dept_id);
+
+-- subquery에서 main query의 table을 사용하도록 정의하면 상관 쿼리.
 
 -- 직원이 한명도 없는 부서의 부서ID(dept.dept_id)와 이름(dept.dept_name), 위치(dept.loc)를 조회
 
-
+select d.dept_id, d.dept_name, d.loc
+from   dept d
+where  not exists (select * from emp e where e.dept_id = d.dept_id);
 
--- 03_SQL/06_DML.sql
+++ 03_SQL/06_DML.sql
@@ -19,31 +19,68 @@ SET    변경할 컬럼 = 변경할 값  [, 변경할 컬럼 = 변경할 값]
  - SET: 변경할 컬럼과 값을 지정
  - WHERE: 변경할 행을 선택. 
 ************************************************************************ */
+use hr_join;
 
 -- 직원 ID가 200인 직원의 급여를 5000으로 변경
 
+select * from emp where emp_id = 200;
+
+update emp
+set salary = 5000
+where emp_id = 200; 
 
 -- 직원 ID가 200인 직원의 급여를 10% 인상한 값으로 변경.
 
+-- salary = 5000
+-- salary = salary * 1.1
+update emp
+set    salary = salary * 1.1
+where emp_id = 200;
 
 -- 부서 ID가 100인 직원의 커미션 비율을 null 로 변경.
 
+select * from emp where dept_id = 100;
+update emp
+set    comm_pct = 0.1
+where  dept_id = 100;
+
+update emp
+set    comm_pct = null  #변경: is null을 사용안함.(where절에서는 is null로 비교)
+where  dept_id = 100;
+
 
 --  부서 ID가 100인 직원들의 급여를 100% 인상
+select * from emp where dept_id = 100;
 
-
+update emp
+set    salary = salary * 2
+where  dept_id = 100;
 
 -- 부서 ID가 100인 직원의 커미션 비율을 0.2로 salary는 3000을 더한 값으로, 상사_id는 100 변경.
 
+update emp
+set    comm_pct = 0.2,
+	   salary = salary + 3000,
+       mgr_id = 100
+where  dept_id = 100;
 
-
+select * from emp where dept_id = 100;
 
 --  IT 부서의 직원들의 급여를 3배 인상
 
-
+select * from emp where dept_id = 60;
+update emp
+set    salary = salary * 3
+where  dept_id in (select dept_id from dept where dept_name = 'IT'); 
 
 -- EMP 테이블의 모든 데이터를 MGR_ID는 NULL로 HIRE_DATE 는 현재일시로 COMM_PCT는 0.5로 수정.
 
+select * from emp;
+update emp
+set    mgr_id = null,
+       hire_date = curdate(),
+       comm_pct = 0.5;
+       
 /* *********************************************************************
 DELETE : 테이블의 행을 삭제
 구문 
@@ -56,16 +93,38 @@ DELETE : 테이블의 행을 삭제
 
 -- 부서테이블에서 부서_ID가 200인 부서 삭제
 
+select * from dept where dept_id = 200;
+
+delete from dept where dept_id = 200;
 
 -- 부서테이블에서 부서_ID가 10인 부서 삭제
+
+select * from dept where dept_id = 10;
+delete from dept where dept_id = 20;
+
+select count(*) from emp where dept_id is null;
+select count(*) from emp where dept_id = 20;
+
+select count(*) from emp;
+select * from emp where job_id = 'IT_PROG';
+
+delete from job where job_id = 'IT_PROG';
+select * from job where job_id = 'IT_PROG';
 
 
 -- 부서 ID가 없는 직원들을 삭제
 
+select * from emp where dept_id is null;
+select count(*) from emp;
+delete from emp where dept_id is null;
 
 -- 담당 업무(emp.job_id)가 'SA_MAN'이고 급여(emp.salary) 가 12000 미만인 직원들을 삭제.
 
+delete from emp where job_id = 'SA_MAN' and salary < 12000;
 
 -- comm_pct 가 null이고 job_id 가 IT_PROG인 직원들을 삭제
 
+delete from emp where comm_pct is null and job_id = 'IT_PROG';
 
+delete from emp;   # 모두 지우기
+select * from emp;
